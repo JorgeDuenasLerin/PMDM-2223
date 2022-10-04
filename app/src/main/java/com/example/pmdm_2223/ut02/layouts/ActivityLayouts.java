1ed2e91244b002650c2d3f614e5c2844c6bb4aac
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-10-04 21:24:00 +0200

    Ejercicios de dos LinearLayouts

---
+++ app/src/main/java/com/example/pmdm_2223/ut02/layouts/ActivityLayouts.java
@@ -0,0 +1,16 @@
+package com.example.pmdm_2223.ut02.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2223.R;
+
+public class ActivityLayouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouts);
+    }
+}
