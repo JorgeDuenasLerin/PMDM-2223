c61640a5ee39943cce49a73c95563e6abaeed240
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-10-18 21:44:29 +0200

    Inicio ejercicio

---
+++ app/src/main/java/com/example/pmdm_2223/ut02/activities/ut02RecogerViaje.java
@@ -0,0 +1,16 @@
+package com.example.pmdm_2223.ut02.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2223.R;
+
+public class ut02RecogerViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ut02_recoger_viaje);
+    }
+}
